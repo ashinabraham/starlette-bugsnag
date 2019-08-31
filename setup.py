400cbf25785c0f6147b3836e9923740a4d9eb92f
Ashin Abraham <ashine@alokin.in>
2019-08-31 15:21:12 +0530

    Added Version

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import re
 
 from setuptools import setup, find_packages
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with open(os.path.join(package, "__init__.py")) as f:
+        return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
+
 
 setup(
     name="starlette_bugsnag",
