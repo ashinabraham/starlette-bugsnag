d643ca9cfc6f9c4b2d27c4a72b5e19698757fa1a
Ashin Abraham <ashine@alokin.in>
2019-08-31 20:29:27 +0530

    Added Long Description in setup file

--- setup.py
+++ setup.py
@@ -14,6 +14,14 @@ def get_version(package):
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
 
 
+def get_long_description():
+    """
+    Return the README.
+    """
+    with open("README.md", encoding="utf8") as f:
+        return f.read()
+
+
 setup(
     name="starlette-bugsnag",
     python_requires=">=3.6",
@@ -23,6 +31,8 @@ setup(
     license="MIT",
     author_email="ashineabraham@gmail.com",
     data_files=[("", ["LICENSE"])],
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=["bugsnag"],
     classifiers=[
